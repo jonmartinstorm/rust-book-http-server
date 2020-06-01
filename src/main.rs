89ccc4bad2f64c40de6cd758ca9653481c9170b1
Jon-Martin Storm <jonmartin.storm@gmail.com>
2020-06-01 20:18:27 +0200

    first webpage

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-
+use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
@@ -15,8 +15,11 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0;512];
-
     stream.read(&mut buffer).unwrap();
+    let contents = fs::read_to_string("hello.html").unwrap();
 
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
